0fb61e4e9c11399e4fa8e0a377d336fd4f11c4be
wuming2 <lxy_4232@163.com>
2015-10-13 08:07:42 +0800

    a

---
+++ whv/src/main/java/com/lxy/test/whv/ui/contact/DiscoverFragment.java
@@ -0,0 +1,11 @@
+package com.lxy.test.whv.ui.contact;
+
+import com.lxy.test.whv.ui.base_activity.BaseFragment;
+
+/**
+ * Created by wuming on 2015/10/13.
+ */
+public class DiscoverFragment extends BaseFragment {
+
+
+}
