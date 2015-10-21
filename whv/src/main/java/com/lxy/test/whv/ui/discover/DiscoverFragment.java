eb2a9227258b033fa3772248b8e047e4e11af62f
wuming <lxy_4232@163.com>
2015-10-21 18:10:43 +0800

    modify diacover ui, no big details

--- whv/src/main/java/com/lxy/test/whv/ui/discover/DiscoverFragment.java
+++ whv/src/main/java/com/lxy/test/whv/ui/discover/DiscoverFragment.java
@@ -5,14 +5,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
-
+import com.lxy.test.whv.App;
 import com.lxy.test.whv.R;
 import com.lxy.test.whv.ui.base_activity.BaseFragment;
 
 import butterknife.ButterKnife;
-import butterknife.InjectView;
 import butterknife.OnClick;
 
 /**
@@ -20,25 +19,46 @@ import butterknife.OnClick;
  */
 public class DiscoverFragment extends BaseFragment {
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.discover_fragment, container, false);
         ButterKnife.inject(this, view);
+
         return view;
+    }
+
+    private void initHeader() {
+        headerLayout.showTitle(App.ctx.getString(R.string.discover_title));
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        initHeader();
     }
 
-    //TODO
     @OnClick(R.id.discover_nearby)
     public void goNearbyActivity() {
         Intent intent = new Intent(this.getActivity(), NearbyActivity.class);
         this.getActivity().startActivity(intent);
+    }
+
+    //TODO
+    @OnClick(R.id.discover_together)
+    public void goTogetherActivity() {
+        Toast.makeText(this.getActivity(), "开发中", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this.getActivity(), NearbyActivity.class);
+//        this.getActivity().startActivity(intent);
+    }
+
+    //TODO
+    @OnClick(R.id.discover_meetup)
+    public void goMeetUp() {
+        Toast.makeText(this.getActivity(), "开发中", Toast.LENGTH_SHORT).show();
+//        Intent intent = new Intent(this.getActivity(), NearbyActivity.class);
+//        this.getActivity().startActivity(intent);
     }
 
     @Override
